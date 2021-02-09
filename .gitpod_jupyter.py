5045e30e7cf7f3ad25df99a1ef59f2d051e2c8c2
robertbuff <robertbuff@mac.com>
2021-02-09 16:42:05 +0000

    Refine gitpod

---
+++ .gitpod_jupyter.py
@@ -0,0 +1,22 @@
+from notebook.auth import passwd
+import random
+import os
+
+random.seed()
+
+password = '{:08d}'.format(random.randint(0,1e8))
+password_encrypted = passwd(password)
+
+command = (
+    "jupyter"
+    " notebook"
+    " --NotebookApp.allow_origin=$(gp url 8888)"
+    " --ip=*"
+    " --NotebookApp.password={}".format(password_encrypted)
+)
+
+print('\n\nClick <Make Public>, <Open Browser> and enter Jupyter password {}\n\n'.format(
+    password
+))
+
+os.system(command)
